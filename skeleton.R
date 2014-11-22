cef0e23eb23dfa3972dbd5fa6352d7cd9af04439
eaHat <ea.chatzimichali@googlemail.com>
2014-11-22 20:06:57 +0000

    Deleted files

--- skeleton.R
+++
@@ -1,8 +0,0 @@
-package.skeleton(name="classyfire", 
-                 code_files=c("bootstrapping.R","initChecks.R", "parallelFunc.R", 
-                              "runRadialComplex.R", "runRBF.R", "parPerm.R", "statFunc.R"))
-
-cd /Library/WebServer/CGI-Executables/newPack/
-R CMD BUILD classyfire
-R CMD CHECK classyfire
-R CMD REMOVE classyfire
